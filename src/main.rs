a68e73086d1e0c741f51bc584b623cad4930da90
鐘妘甄 <shirley@mail.kway.com>
2024-04-08 14:43:05 +0800

    refactor fn

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use crossterm::{
 };
 use ratatui::{
     prelude::{CrosstermBackend, Stylize,Terminal},
-    widgets::Paragraph,
+    widgets::{Block, Borders, Paragraph}, Frame,
 };
 use std::io::{self, Stdout};
 use std::error::Error;
@@ -17,25 +17,34 @@ fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn Error>
     Ok(Terminal::new(CrosstermBackend::new(stdout))?)
 }
 
-fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn Error>> {
-    Ok(loop {
-        terminal.draw(|frame| {
-            let area = frame.size();
-            frame.render_widget(
-                Paragraph::new("Hello Ratatui! (press 'q' to quit)")
-                    .white().on_blue(), area,
-            );
-        })?;
-        if event::poll(std::time::Duration::from_millis(16))? {
-            if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press
-                    && key.code == KeyCode::Char('q')
-                {
-                    break;
-                }
+fn ui(frame: &mut Frame) {
+    frame.render_widget(
+        Paragraph::new("Hello World")
+        .block(Block::default().title("Greeting").borders(Borders::ALL)),
+         frame.size(),
+        );
+}
+
+fn handle_events() -> io::Result<bool> {
+    if event::poll(std::time::Duration::from_millis(16))? {
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press
+                && key.code == KeyCode::Char('q')
+            {
+                return Ok(true);
             }
         }
-    })
+    }
+    return Ok(false);
+}
+
+fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn Error>> {
+    let mut should_quit = false;
+    while !should_quit {
+        terminal.draw(ui)?;
+        should_quit = handle_events()?;
+    }
+    Ok(())
 }
 
 fn restore_terminal(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn Error>> {
